f0a0bb0ca6984e0716812fb8404aa1e443e89a95
Roy Wu <roy32011@gmail.com>
2023-12-05 20:37:58 +0800

    Day 22: Add app description, image, and links

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.png", width=600)
+    st.image("images/photo.png", width=800)
 
 with col2:
     st.title("吳泓儒")
@@ -24,13 +24,19 @@ Below you can find some of the apps I have built in Python. Feel free to contact
 """
 st.write(content2)
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pd.read_csv("data.csv", sep=";")
 with col3:
     for index, row in df[0:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image(f"images/{row['image']}")
+        st.write(f"[Source Code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image(f"images/{row['image']}")
+        st.write(f"[Source Code]({row['url']})")
