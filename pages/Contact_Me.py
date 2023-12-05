228805e13f929fc51e06e6db37ce382fda7bd1db
Roy Wu <roy32011@gmail.com>
2023-12-05 22:12:10 +0800

    Day 23: Send email

--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -1,3 +1,13 @@
 import streamlit as st
 
-st.header("Contact Me")
+st.set_page_config(layout="wide")
+
+st.header("聯絡我 Contact Me")
+
+with st.form(key="email_form"):
+    user_email = st.text_input("您的信箱")
+    message = st.text_area("您想詢問的問題")
+    button = st.form_submit_button(label="提交")
+    if button:
+        pass
+        
