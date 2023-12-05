a5530c81b8cbcb316b7437761de27006c38f9e56
Roy Wu <roy32011@gmail.com>
2023-12-05 20:27:19 +0800

    Day 22: Render the app titles in two columns

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.png")
+    st.image("images/photo.png", width=600)
 
 with col2:
     st.title("吳泓儒")
@@ -22,3 +23,14 @@ content2 = """
 Below you can find some of the apps I have built in Python. Feel free to contact me!
 """
 st.write(content2)
+
+col3, col4 = st.columns(2)
+
+df = pd.read_csv("data.csv", sep=";")
+with col3:
+    for index, row in df[0:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
