54a6f4eb26ac4067818d10a64cb7654ebea5e376
roywu <roy32011@gmail.com>
2023-12-06 09:37:27 +0800

    Day 23: Send emails using the form

--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from send_email import send_email
 
 st.set_page_config(layout="wide")
 
@@ -6,8 +7,13 @@ st.header("聯絡我 Contact Me")
 
 with st.form(key="email_form"):
     user_email = st.text_input("您的信箱")
-    message = st.text_area("您想詢問的問題")
+    raw_message = st.text_area("您想詢問的問題")
+    message = f"""\
+Subject: New email from {user_email}
+From: {user_email}
+{raw_message}
+"""
     button = st.form_submit_button(label="提交")
     if button:
-        pass
-        
+        send_email(message)
+        st.info("成功寄出信件！")
--- send_email.py
+++ send_email.py
@@ -1,21 +1,17 @@
 import os
 import smtplib, ssl
 
-host = "smtp.gmail.com"
-port = 465
 
-username = "roy32011@gmail.com"
-password = "koyz csvu izgi awmd"
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
 
-receiver = "roy32011@gmail.com"
-context = ssl.create_default_context()
+    username = "roy32011@gmail.com"
+    password = "koyz csvu izgi awmd"
 
-message = """\
-Subject: Hi!
-How are you?
-Bye!
-"""
+    receiver = "roy32011@gmail.com"
+    context = ssl.create_default_context()
 
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
